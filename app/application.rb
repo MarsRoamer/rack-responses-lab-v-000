53432e6d7628698ae50a6d78dff0db08a355b605
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-13 16:28:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,9 @@
 class Application
 
   def call(env)
+    time = Time.new
     resp = Rack::Response.new
-    resp.write "#{Time.hour}:#{Time.min}"
+    resp.write "#{time.hour}:#{time.min}"
     resp.finish
   end
 
